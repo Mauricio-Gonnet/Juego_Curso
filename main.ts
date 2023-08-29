82ede1d6d4711e365bcbbbcb4a3e293af5d201ab
Mauricio-Gonnet <122744079+Mauricio-Gonnet@users.noreply.github.com>
2023-08-29 19:59:55 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,37 @@
+input.onButtonPressed(Button.A, function () {
+    Pez.move(-1)
+})
+input.onButtonPressed(Button.B, function () {
+    Pez.move(1)
+})
+let Pez: game.LedSprite = null
+basic.showNumber(3)
+basic.pause(1000)
+basic.showNumber(2)
+basic.pause(1000)
+basic.showNumber(1)
+basic.pause(1000)
+Pez = game.createSprite(0, 0)
+let Red = game.createSprite(2, 4)
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    for (let index = 0; index < 4; index++) {
+        Red.change(LedSpriteProperty.Y, 1)
+        basic.pause(200)
+    }
+    for (let index = 0; index < 4; index++) {
+        Red.change(LedSpriteProperty.Y, -1)
+        basic.pause(200)
+    }
+})
+basic.forever(function () {
+    if (Pez.isTouching(Red)) {
+        game.gameOver()
+        basic.showNumber(game.score())
+        basic.pause(500)
+    } else {
+    	
+    }
+})
