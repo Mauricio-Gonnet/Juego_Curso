82ad3f1e9efc2f2d0b215194213bb9556d8ce128
Mauricio-Gonnet <122744079+Mauricio-Gonnet@users.noreply.github.com>
2023-09-03 12:55:59 -0300

    Sistema de puntación

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+/**
+ * Juego el pescador:
+ * 
+ * El pez, para mover el pez hacia la derecha debes precionar el botón B y para moverlo hacia la izquierda el botón A
+ * 
+ * Para ganar puntos debes tocar los bordes del panel led y si  la red atrapa al pez, perderás.
+ */
 input.onButtonPressed(Button.A, function () {
     Pez.move(-1)
 })
@@ -11,11 +18,8 @@ basic.showNumber(2)
 basic.pause(1000)
 basic.showNumber(1)
 basic.pause(1000)
-Pez = game.createSprite(0, 0)
+Pez = game.createSprite(3, 2)
 let Red = game.createSprite(2, 4)
-basic.forever(function () {
-	
-})
 basic.forever(function () {
     for (let index = 0; index < 4; index++) {
         Red.change(LedSpriteProperty.Y, 1)
@@ -30,8 +34,20 @@ basic.forever(function () {
     if (Pez.isTouching(Red)) {
         game.gameOver()
         basic.showNumber(game.score())
-        basic.pause(500)
-    } else {
-    	
+        basic.pause(100)
+    }
+})
+basic.forever(function () {
+    if (Pez.get(LedSpriteProperty.X) == 4) {
+        game.addScore(1)
+        basic.pause(1000)
+        Pez.set(LedSpriteProperty.X, 3)
+    }
+})
+basic.forever(function () {
+    if (Pez.get(LedSpriteProperty.X) == 0) {
+        game.addScore(1)
+        basic.pause(1000)
+        Pez.set(LedSpriteProperty.X, 1)
     }
 })
